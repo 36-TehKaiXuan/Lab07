abee0942eebb9b85cb2228778df97a7397379213
36_TehKaiXuan <91871965+36-TehKaiXuan@users.noreply.github.com>
2021-10-19 19:03:35 +0800

    Scene Added with Jump Feature for the Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,30 @@ public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    //Jump
+    public int JumpForce;
+    private Rigidbody rb;
+
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        //Jump
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Make the bird "Fly"
+            //transform.position += transform.up * JumpForce * Time.deltaTime;
+            rb.AddForce(Vector3.up * JumpForce);
+
+            //Animation
             thisAnimation.Play();
+        }
+            
     }
 }
